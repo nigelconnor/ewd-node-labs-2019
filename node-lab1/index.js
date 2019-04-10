8d4f135a191392c65fad6e242cd84c808627f726
Nigel O'Connor <treatyman@gmail.com>
2019-04-10 12:20:35 +0100

    added routing for hacker news API

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import contactsRouter from './api/contacts';
 import bodyParser from 'body-parser';
+import postsRouter from './api/posts';
 
 dotenv.config();
 
@@ -9,12 +10,15 @@ const app = express();
 
 const port = process.env.PORT;
 
+
+
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
 
+app.use('/api/posts', postsRouter);
 app.use('/api/contacts', contactsRouter);
 app.use(express.static('public'));
 
